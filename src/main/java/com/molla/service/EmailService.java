0578c7102ee6097e280267d0635f535633db2b75
Trinh Huu Thien An <thienan8928@gmail.com>
2023-05-21 11:13:09 +0700

    update register

--- src/main/java/com/molla/service/EmailService.java
+++ src/main/java/com/molla/service/EmailService.java
@@ -1,11 +1,9 @@
 package com.molla.service;
 
-import com.molla.model.EmailInfo;
-
 import jakarta.mail.MessagingException;
 
 public interface EmailService {
-//	public void sendMail(EmailInfo emailInfo) throws MessagingException;
 	public void sendMail(String from, String to, String subject, String message) throws MessagingException;
+
 	public String buildEmail(String name, String link);
 }
