8a751a1abbec86fbc89a2148932c886fd98f28df
Dew <84929737+taipc18@users.noreply.github.com>
2023-06-01 22:54:08 +0700

    Display product on home page

--- src/main/java/com/molla/controller/HomeController.java
+++ src/main/java/com/molla/controller/HomeController.java
@@ -1,14 +1,26 @@
 package com.molla.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.molla.model.Product;
+import com.molla.service.ProductService;
+
 @Controller
 public class HomeController {
+	@Autowired
+	ProductService productService;
+	
 	
 	@RequestMapping("/home")
 	public String home(Model model) {
+		List<Product> list = productService.findAll();
+		model.addAttribute("items", list);
 		return "client/layout/index";
 	}
+	
 }
