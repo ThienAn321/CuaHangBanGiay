ce0b65eeea4cf3fecfb033d39b51b3c3be33f3b7
Trinh Huu Thien An <thienan8928@gmail.com>
2023-06-01 16:00:37 +0700

    update cart and checkout

---
+++ src/main/java/com/molla/controller/CartController.java
@@ -0,0 +1,18 @@
+package com.molla.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class CartController {
+	@RequestMapping("/cart")
+	public String cart() {
+		return "client/cart/cart";
+	}
+	
+	@RequestMapping("/checkout")
+	public String checkout() {
+		return "client/cart/checkout";
+	}
+}
